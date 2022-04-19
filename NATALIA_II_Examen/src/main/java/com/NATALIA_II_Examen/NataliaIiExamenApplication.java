50f432efbec017ef77e8791708dc04c0e86b8dc2
Natalia Abarca <98279086+la-vaca-muuuh@users.noreply.github.com>
2022-04-18 20:22:38 -0600

    Add files via upload
    Commit del examen

---
+++ NATALIA_II_Examen/src/main/java/com/NATALIA_II_Examen/NataliaIiExamenApplication.java
@@ -0,0 +1,13 @@
+package com.NATALIA_II_Examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NataliaIiExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NataliaIiExamenApplication.class, args);
+	}
+
+}
---
+++ NATALIA_II_Examen/src/test/java/com/NATALIA_II_Examen/NataliaIiExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NATALIA_II_Examen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NataliaIiExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
